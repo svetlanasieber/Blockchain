d39a9fb9c3e14e37d8fb4e80df053204d31af927
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-09-21 23:31:52 +0200

    Update Block.java

--- SimpleBlockchain/Block.java
+++ SimpleBlockchain/Block.java
@@ -1,1 +1,27 @@
+package SimpleBlockchain;
 
+
+
+import java.util.Arrays;
+
+public class Block {
+    private final int previousHash;
+    private final String[] transactions;
+
+    public Block(int previousHash, String[] transactions) {
+        this.previousHash = previousHash;
+        this.transactions = transactions;
+
+
+        Object[] contens = {Arrays.hashCode(transactions), previousHash};
+        int blockHash = Arrays.hashCode(contens);
+    }
+    public int getPreviousHash() {
+        return previousHash;
+    }
+    public String[] getTransaction() {
+        return transactions;
+    }
+
+
+}
